48dc4c41fc581a56e99ef4738d0b8d154b708080
mattkusman <mattkusman@gmial.com>
2022-09-02 15:53:21 -0700

    Updates

---
+++ precache-manifest.2426d5ffe228973fe26a0e44a815f0a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a7f499a9881ea0ebb5a",
+    "url": "/static/css/main.fcb42f75.chunk.css"
+  },
+  {
+    "revision": "9a7f499a9881ea0ebb5a",
+    "url": "/static/js/main.9a7f499a.chunk.js"
+  },
+  {
+    "revision": "05b503beb4d93191790c",
+    "url": "/static/js/1.05b503be.chunk.js"
+  },
+  {
+    "revision": "24b7e0439a8c07984cdd",
+    "url": "/static/js/2.24b7e043.chunk.js"
+  },
+  {
+    "revision": "e74a177ff13a652a2f2c",
+    "url": "/static/js/runtime~main.e74a177f.js"
+  },
+  {
+    "revision": "bb4932d95f2af1f188da5e57e4923856",
+    "url": "/index.html"
+  }
+];
